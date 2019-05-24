66a3f61c5a8b02ab738dc5e5f21b79ea3aba20a5
cv <cvaleonline@gmail.com>
2019-05-24 14:19:53 +0200

    pin assignments

---
+++ 10lights-src/10lights-src.ino
@@ -0,0 +1,101 @@
+/*
+ *  10 lights
+ *  -------------
+ *  Summer project with Stu, lighting console for 10 LEDs
+ *
+ *  Code by Carlos Valente
+ *  
+ *  TODO:
+ *  Pin definitions
+ *  - define pin type https://www.arduino.cc/en/Tutorial/DigitalPins
+ *  - define pin names from table 
+ */
+#include "def.h"
+
+/* Gen - State Machine */
+enum states {MODE_1, MODE_2, MODE_3};
+uint8_t state = MODE_1;
+
+/* Gen - Lighting States */
+uint8_t currentCue[NUM_LIGHTS + 1];   // numLights + cue fade time
+uint8_t previousCue[NUM_LIGHTS + 1];
+uint8_t nextCue[NUM_LIGHTS + 1];
+uint8_t currentLighting[NUM_LIGHTS];
+
+/* Gen - Pin Assignments */
+uint8_t analogInputs[] {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10};     // Faders, last is master
+uint8_t digitalInputs[] {40, 52, 53};                                     // store, back, go
+uint8_t digitalOutputsPWM[] {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};         // channel indicator LED, first is master
+uint8_t digitalOutputs[] {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};            // cue indicator LED, 
+
+void setup(){
+
+    /* Pin Assignments
+     * Pin Name is MEGA name, the long way
+     */
+    
+    int arraySize;
+
+    // Pin Assignments - Analog Pins
+    arraySize = sizeof(analogInputs) / sizeof(analogInputs[0]);
+    for (int i = 0; i < arraySize; i++) {
+        pinMode(analogPins[i], INPUT);
+    }
+
+    // Pin Assignments - Digital Pins INPUT
+    arraySize = sizeof(digitalInputs) / sizeof(digitalInputs[0]);
+    for (int i = 0; i < arraySize; i++) {
+        pinMode(digitalInputs[i], INPUT);
+    }
+
+    // Pin Assignments - Digital Pins PWM OUTPUT
+    arraySize = sizeof(digitalOutputsPWM) / sizeof(digitalOutputsPWM[0]);
+    for (int i = 0; i < arraySize; i++) {
+        pinMode(digitalOutputsPWM[i], OUTPUT);
+    }
+
+    // Pin Assignments - Digital Pins OUTPUT
+    arraySize = sizeof(digitalOutputs) / sizeof(digitalOutputs[0]);
+    for (int i = 0; i < arraySize; i++) {
+        pinMode(digitalOutputs[i], OUTPUT);
+    }
+
+    // Serial setup
+    Serial.begin(115200);
+
+}
+
+void loop(){
+    read_inputs();                  // this should only happen if needed
+    loop_execute(check_mode());
+
+}
+
+void read_inputs(){
+    // reads declared inputs
+}
+
+uint8_t check_mode(){
+    // reads mode selection input
+
+}
+
+void loop_execute(){
+    switch (state) {
+
+        case MODE_1:
+            // Fader is value
+            Serial.println("Mode 1");
+        break;
+
+        case MODE_2:
+            // Record
+            Serial.println("Mode 2");
+        break;
+        case MODE_3:
+            // Playback
+            Serial.println("Mode 3");
+        break;
+    }
+}
+
---
+++ 10lights-src/def.h
@@ -0,0 +1,21 @@
+#ifndef DEF_H
+#define DEF_H
+
+/* Gen */
+
+/* Gen - SW Debounce */
+#define DEBOUNCE_TIME 200
+#define WARM_UP_TIME 500
+
+/* Serial */
+#define SERIAL_BAUD 115200
+
+/* EEPROM */
+#include <Wire.h>
+#define disk1 0x50          // !! check for mega
+#define EEPROM_ADDRESS 0
+
+/* Project */
+#define NUM_LIGHTS 10
+
+#endif
