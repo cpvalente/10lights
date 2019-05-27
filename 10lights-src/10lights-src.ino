88d222f5a4126e59a3d6d78e442af13ce5c81197
cv <cvaleonline@gmail.com>
2019-05-27 23:55:46 +0200

    mode 2

--- 10lights-src/10lights-src.ino
+++ 10lights-src/10lights-src.ino
@@ -10,6 +10,9 @@
  *  - Modes pass values as reference
  *  Pin definitions
  *  - pins as port 
+ *  LEDs
+ *  - duty cycle for cue leds 
+ *  -- https://www.reddit.com/r/arduino/comments/5pycxp/adjusting_frequency_and_duty_cycle_of_led_blink/
  */
 #include "def.h"
 
@@ -22,18 +25,23 @@ uint8_t currentCue[NUM_FADERS];   // numLights + cue fade time
 uint8_t previousCue[NUM_FADERS];
 uint8_t nextCue[NUM_FADERS];
 uint8_t currentLighting[NUM_LIGHTS];
+uint8_t selectedCue;
 
 /* Gen - Pin Assignments */
-uint8_t analogInputs[]      {A10, A8, A6, A5, A4, A3, A2, A1, A0, A7, A9}; // Faders, first is master
-uint8_t digitalOutputsPWM[] {2,   12, 11, 10,  9,  8,  7 , 6,  5,  4,  3}; // channel indicator LED, first is master
-uint8_t digitalOutputs[]    {22, 23, 24, 25, 26, 27, 28, 29, 30, 31};      // cue indicator LED last
-uint8_t digitalInputs[]     {40, 52, 53};                                  // store, back, go
+const uint8_t analogInputs[]      {A10, A8, A6, A5, A4, A3, A2, A1, A0, A7, A9}; // Faders, first is master
+const uint8_t digitalOutputsPWM[] {2,   12, 11, 10,  9,  8,  7 , 6,  5,  4,  3}; // channel indicator LED, first is master
+const uint8_t digitalOutputs[]    {22, 23, 24, 25, 26, 27, 28, 29, 30, 31};      // cue indicator LED last
+const uint8_t digitalInputs[]     {40, 52, 53};                                  // store, back, go
 
+/* Gen - Pin Values Buttons */
+byte store, back, go;
+byte prevStore, prevBack, prevGo;
+long lastStore, lastBack, lastGo, timeNow;
 
-/* Gen - Pin Values */
-uint8_t store, back, go;
+/* Gen - Pin Values Faders */
 uint8_t faderValues[NUM_FADERS];
 uint8_t values[NUM_FADERS];
+byte    leds[NUM_CUES];
 
 
 void setup(){
@@ -74,9 +82,11 @@ void setup(){
 }
 
 void loop(){
+    timeNow = millis();             // get iteration time
     read_inputs();                  // this should only happen if needed
-    loop_execute( check_mode() );
-    delay(20);
+    loop_execute( check_mode() );   // call state machine
+    write_to_leds();                // write outputs
+    delay(20);                      // !! replace with refresh rate
 }
 
 void read_inputs(){
@@ -107,7 +117,6 @@ void read_inputs(){
 
 void write_to_leds(){
     // writes values to PWM digital
-    // LEDS have opposite direction to inputs
     Serial.println("Writing to PWM outputs inputs...");
     for (int i = 0; i < NUM_LIGHTS; i++) {
         analogWrite(digitalOutputsPWM[i], values[i]);
@@ -115,25 +124,73 @@ void write_to_leds(){
         Serial.print(": ");
         Serial.println(values[i]);
     }
+
+    for (int i = 0; i < NUM_CUES; i++) {
+        // replace by duty cycle 
+        digitalWrite(digitalOutputs[i], leds[i]);
+    }
 }
 
 uint8_t check_mode(){
     // reads mode selection input
-    return 0;
+    uint8_t called_mode = 11;
+
+    if (store) {
+        if (prevStore) {
+            if (timeNow - lastStore > STORE_TIME) {
+                if (state == MODE_1) called_mode = MODE_2;
+                else called_mode = MODE_3;
+            }
+        } else {
+            prevStore = true;
+            lastStore = timeNow;
+        }
+    }
+
+    // go backwards
+    if (back && !prevBack) { 
+        selectedCue -= 1;
+        if (selectedCue >= 10) {
+            selectedCue = 9;
+        }
+        prevBack = true;
+    }
+    if (!back) prevBack = false;
+
+    // go forwards
+    if (go && !prevGo) {
+        selectedCue = (selectedCue + 1) % NUM_CUES;
+        prevGo = true;
+    }
+    if (!go) prevGo = false;
+
+    Serial.print("Selected Cue: ");
+    Serial.println(selectedCue);
+    Serial.print("Selected Mode: ");
+    Serial.println(called_mode);
+
+    return called_mode;
 }
 
 void loop_execute(uint8_t called_mode){
+
+    if (called_mode < 3) state = called_mode;
+
     switch (state) {
 
         case MODE_1:
             // Fader is value
             Serial.println("Mode 1");
 
-            // calculate values to pass, mind order
+            // calculate values to pass
             for (int i = 1; i < NUM_LIGHTS; i++) {
                 values[i] = cap(faderValues[i], faderValues[0]);
             }
-            values[0] = faderValues[0];
+            values[0] = faderValues[0]; // master not affected
+
+            for (int i = 0; i < NUM_CUES; i++) {
+                values[i] = (selectedCue == i); // does this work?
+            }
 
         break;
 
@@ -147,5 +204,4 @@ void loop_execute(uint8_t called_mode){
             Serial.println("Mode 3");
         break;
     }
-    write_to_leds();            // pass the values here
 }
