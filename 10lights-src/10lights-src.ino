b8637e6c0858ac22d0a771562962c7d0a27e00f3
cv <cvaleonline@gmail.com>
2019-05-30 21:27:14 +0200

    changed input pins

--- 10lights-src/10lights-src.ino
+++ 10lights-src/10lights-src.ino
@@ -31,7 +31,7 @@ uint8_t selectedCue;
 const uint8_t analogInputs[]      {A10, A8, A6, A5, A4, A3, A2, A1, A0, A7, A9}; // Faders, first is master
 const uint8_t digitalOutputsPWM[] {2,   12, 11, 10,  9,  8,  7 , 6,  5,  4,  3}; // channel indicator LED, first is master
 const uint8_t digitalOutputs[]    {22, 27, 23, 28, 24, 29, 25, 30, 26, 31};      // cue indicator LED last
-const uint8_t digitalInputs[]     {40, 52, 53};                                  // store, back, go
+const uint8_t digitalInputs[]     {13, 52, 53};                                  // store, back, go
 
 /* Gen - Pin Values Buttons */
 byte store, back, go;
