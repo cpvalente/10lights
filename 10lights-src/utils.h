b8446109924cbc72a13b64df3d0321064803d403
cv <cvaleonline@gmail.com>
2019-05-31 22:52:02 +0200

    potential bug fix

--- 10lights-src/utils.h
+++ 10lights-src/utils.h
@@ -7,6 +7,6 @@ inline uint8_t cap(uint8_t value, uint8_t master){
 }
 
 inline uint8_t largest(uint8_t v1, uint8_t v2){
-    if (v1 > v2) return v1;
+    if (v1 >= v2) return v1;
     else return v2;
 }
